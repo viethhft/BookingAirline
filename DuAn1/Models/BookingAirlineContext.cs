e230dd565caaa238266eb279fa5bebf66e45b8b2
Viet Nguyen <vietndph29972@fpt.edu.vn>
2023-07-18 16:38:27 +0700

    a

--- DuAn1/Models/BookingAirlineContext.cs
+++ DuAn1/Models/BookingAirlineContext.cs
@@ -39,7 +39,7 @@ public partial class BookingAirlineContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DUONGPHAM\\SQLEXPRESS;Initial Catalog=BookingAirline;Integrated Security=True;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Initial Catalog=BookingAirline;Integrated Security=True;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
