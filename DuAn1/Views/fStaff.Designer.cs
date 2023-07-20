1b33dd380e86eff1198351557394156ab1f9c1d6
Thanh Binh <phungthanhbinh594@gmail.com>
2023-07-20 08:22:49 +0700

    Update menu trip

--- DuAn1/Views/fStaff.Designer.cs
+++ DuAn1/Views/fStaff.Designer.cs
@@ -31,31 +31,34 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fStaff));
             pictureBox1 = new PictureBox();
             panel1 = new Panel();
-            button6 = new Button();
             button5 = new Button();
             button4 = new Button();
             button3 = new Button();
             button2 = new Button();
             button1 = new Button();
             pn_chil = new Panel();
+            menuStrip1 = new MenuStrip();
+            hệThốngToolStripMenuItem = new ToolStripMenuItem();
+            đăngXuấtToolStripMenuItem = new ToolStripMenuItem();
+            đổiMậtKhẩuToolStripMenuItem = new ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             panel1.SuspendLayout();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // pictureBox1
             // 
             pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
             pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
-            pictureBox1.Location = new Point(3, 1);
+            pictureBox1.Location = new Point(3, 27);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(1277, 179);
+            pictureBox1.Size = new Size(1277, 153);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
             // panel1
             // 
             panel1.BorderStyle = BorderStyle.FixedSingle;
-            panel1.Controls.Add(button6);
             panel1.Controls.Add(button5);
             panel1.Controls.Add(button4);
             panel1.Controls.Add(button3);
@@ -66,20 +69,10 @@
             panel1.Size = new Size(200, 522);
             panel1.TabIndex = 1;
             // 
-            // button6
-            // 
-            button6.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            button6.Location = new Point(3, 461);
-            button6.Name = "button6";
-            button6.Size = new Size(194, 48);
-            button6.TabIndex = 5;
-            button6.Text = "Hệ thống";
-            button6.UseVisualStyleBackColor = true;
-            // 
             // button5
             // 
             button5.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            button5.Location = new Point(3, 377);
+            button5.Location = new Point(1, 418);
             button5.Name = "button5";
             button5.Size = new Size(194, 48);
             button5.TabIndex = 4;
@@ -90,7 +83,7 @@
             // button4
             // 
             button4.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            button4.Location = new Point(3, 289);
+            button4.Location = new Point(1, 330);
             button4.Name = "button4";
             button4.Size = new Size(194, 48);
             button4.TabIndex = 3;
@@ -100,7 +93,7 @@
             // button3
             // 
             button3.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            button3.Location = new Point(3, 193);
+            button3.Location = new Point(1, 234);
             button3.Name = "button3";
             button3.Size = new Size(194, 48);
             button3.TabIndex = 2;
@@ -110,7 +103,7 @@
             // button2
             // 
             button2.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            button2.Location = new Point(3, 107);
+            button2.Location = new Point(1, 148);
             button2.Name = "button2";
             button2.Size = new Size(194, 48);
             button2.TabIndex = 1;
@@ -120,7 +113,7 @@
             // button1
             // 
             button1.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            button1.Location = new Point(3, 19);
+            button1.Location = new Point(1, 60);
             button1.Name = "button1";
             button1.Size = new Size(194, 48);
             button1.TabIndex = 0;
@@ -134,6 +127,34 @@
             pn_chil.Size = new Size(1071, 522);
             pn_chil.TabIndex = 2;
             // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { hệThốngToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1284, 24);
+            menuStrip1.TabIndex = 3;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // hệThốngToolStripMenuItem
+            // 
+            hệThốngToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { đăngXuấtToolStripMenuItem, đổiMậtKhẩuToolStripMenuItem });
+            hệThốngToolStripMenuItem.Name = "hệThốngToolStripMenuItem";
+            hệThốngToolStripMenuItem.Size = new Size(69, 20);
+            hệThốngToolStripMenuItem.Text = "Hệ thống";
+            // 
+            // đăngXuấtToolStripMenuItem
+            // 
+            đăngXuấtToolStripMenuItem.Name = "đăngXuấtToolStripMenuItem";
+            đăngXuấtToolStripMenuItem.Size = new Size(145, 22);
+            đăngXuấtToolStripMenuItem.Text = "Đăng xuất";
+            // 
+            // đổiMậtKhẩuToolStripMenuItem
+            // 
+            đổiMậtKhẩuToolStripMenuItem.Name = "đổiMậtKhẩuToolStripMenuItem";
+            đổiMậtKhẩuToolStripMenuItem.Size = new Size(145, 22);
+            đổiMậtKhẩuToolStripMenuItem.Text = "Đổi mật khẩu";
+            // 
             // fStaff
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -142,25 +163,33 @@
             Controls.Add(pn_chil);
             Controls.Add(panel1);
             Controls.Add(pictureBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
             Margin = new Padding(3, 2, 3, 2);
             Name = "fStaff";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "fStaff";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             panel1.ResumeLayout(false);
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private PictureBox pictureBox1;
         private Panel panel1;
-        private Button button6;
         private Button button5;
         private Button button4;
         private Button button3;
         private Button button2;
         private Button button1;
         private Panel pn_chil;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem hệThốngToolStripMenuItem;
+        private ToolStripMenuItem đăngXuấtToolStripMenuItem;
+        private ToolStripMenuItem đổiMậtKhẩuToolStripMenuItem;
     }
 }
