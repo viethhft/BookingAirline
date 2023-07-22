6c661d8d78a5585453bf71fc85aa4f1bbabcd6fe
Viet Nguyen <vietndph29972@fpt.edu.vn>
2023-07-23 02:48:32 +0700

    plane

---
+++ 1_DAL/Repon/PlaneTypeRepo.cs
@@ -0,0 +1,23 @@
+﻿using _1_DAL.IRepon;
+using _1_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_DAL.Repon
+{
+    public class PlaneTypeRepo : IPlaneTypeRepo
+    {
+        BookingAirlineContext _context;
+        public PlaneTypeRepo()
+        {
+            _context=new BookingAirlineContext();
+        }
+        public List<PlaneType> get_list()
+        {
+            return _context.PlaneTypes.ToList();
+        }
+    }
+}
---
+++ 2_BUS/IService/IPlaneTypeServices.cs
@@ -0,0 +1,14 @@
+﻿using _1_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_BUS.IService
+{
+    public interface IPlaneTypeServices
+    {
+        public List<PlaneType> get_list();
+    }
+}
