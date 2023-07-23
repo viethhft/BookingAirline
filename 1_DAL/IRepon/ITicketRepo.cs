a051fd3cab2314b6a1460550fe76cfe6dc1e672a
Viet Nguyen <vietndph29972@fpt.edu.vn>
2023-07-24 05:41:26 +0700

    q

---
+++ 1_DAL/IRepon/ITicketRepo.cs
@@ -0,0 +1,17 @@
+﻿using _1_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_DAL.IRepon
+{
+    public interface ITicketRepo
+    {
+        public List<Ticket> GetTickets();
+        public bool UpdateTicket(Ticket ticket);
+        public bool AddTicket(Ticket ticket);
+
+    }
+}
--- DuAn1/Views/View User/FthongTinNguoiDung.cs
+++ DuAn1/Views/View User/FthongTinNguoiDung.cs
@@ -1,4 +1,5 @@
-﻿using _2_BUS.IService;
+﻿using _1_DAL.Models;
+using _2_BUS.IService;
 using _2_BUS.Service;
 using System;
 using System.Collections.Generic;
@@ -14,7 +15,7 @@ namespace GUI.Views.View_User
 {
     public partial class FthongTinNguoiDung : Form
     {
-        private string _message="";
+        private string _message = "";
 
         ICustomerServices _service;
 
@@ -22,37 +23,29 @@ namespace GUI.Views.View_User
         {
             _service = new CustomerServices();
             InitializeComponent();
-            cbx_gt.Items.Add("Nam");
-            cbx_gt.Items.Add("Nữ");
-            load();
-
         }
 
 
         public FthongTinNguoiDung(string message) : this()
         {
             _message = message;
+            cbx_gt.Items.Add("Nam");
+            cbx_gt.Items.Add("Nữ");
+            _service = new CustomerServices();
+            load();
+           
         }
 
         public void load()
         {
-            foreach (var item in _service.GetCustomers())
-            {
-                
-                    tbx_email.Text = item.Email;
-                    tbx_diaChi.Text = item.Address;
-                    tbx_hoTen.Text = item.FirstName+" "+ item.MiddleName+" "+ item.LastName;
-                    if (item.Gender == "Nam")
-                    {
-                        cbx_gt.Text = "Nam";
-                    }
-                    else
-                    {
-                        cbx_gt.Text = "Nữ";
-                    }
-                    date_bird.Value = (DateTime)(item.Dob);
-                    tbx_sdt.Text = item.Phone;
-            }
+            Customer item = _service.GetCustomers().Where(c => c.Email == _message).FirstOrDefault();
+
+            tbx_email.Text = item.Email;
+            tbx_diaChi.Text = item.Address;
+            tbx_hoTen.Text = item.FirstName + " " + item.MiddleName + " " + item.LastName;
+            cbx_gt.Text = item.Gender;
+            date_bird.Value = (DateTime)(item.Dob);
+            tbx_sdt.Text = item.Phone;
         }
 
 
