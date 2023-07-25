52681875bb9bf005fd79ee3f7913a915b7bfe927
Thanh Binh <phungthanhbinh594@gmail.com>
2023-07-26 01:13:57 +0700

    a

--- 1_DAL/Models/Flight.cs
+++ 1_DAL/Models/Flight.cs
@@ -21,6 +21,9 @@ namespace _1_DAL.Models
         public DateTime DateFlight { get; set; }
         public DateTime? DateTo { get; set; }
 
+        public DateTime timeStart { get;set; }
+        public DateTime timeEnd { get;set; }
+
         public virtual Airport Airport { get; set; } = null!;
         public virtual PlaneType PlaneType { get; set; } = null!;
         public virtual ICollection<Recommend> Recommends { get; set; }
