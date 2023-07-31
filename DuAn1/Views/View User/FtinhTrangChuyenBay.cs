f8c82f343a345c28a1d4a3f33de08dade26589f9
Viet Nguyen <vietndph29972@fpt.edu.vn>
2023-08-01 02:34:58 +0700

    a

--- DuAn1/Views/View User/FtinhTrangChuyenBay.cs
+++ DuAn1/Views/View User/FtinhTrangChuyenBay.cs
@@ -175,7 +175,6 @@ namespace GUI.Views.View_User
                         {
                             DateTime date = new DateTime(date_Start.Value.Year, date_Start.Value.Month, date_Start.Value.Day);
                             var list_search = _flightServices.get_list().Where(c => c.GoFrom == cbb_From.Text && c.GoTo == cbb_To.Text && c.DateFlight == date).ToList();
-                        MessageBox.Show(list_search.Count.ToString());
                             FTinhTrangChuyenBayHanhTrinhChild hanhtrinh = new FTinhTrangChuyenBayHanhTrinhChild(list_search);
                             this.Hide();
                             hanhtrinh.ShowDialog();
