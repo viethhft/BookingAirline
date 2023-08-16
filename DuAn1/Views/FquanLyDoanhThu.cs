a151a828307ed93d6f35aca0957f4a8ac69e2373
Thanh Binh <phungthanhbinh594@gmail.com>
2023-08-17 03:09:32 +0700

    a

--- DuAn1/Views/FquanLyDoanhThu.cs
+++ DuAn1/Views/FquanLyDoanhThu.cs
@@ -93,20 +93,27 @@ namespace DuAn1.Views
                         }
                         else
                         {
-                            var fl = _flightServices.get_list().Where(c => c.Id == ticket[i].FlightId).FirstOrDefault();
-                            var plane = _planeTypeServices.get_list().Where(c => c.Id == fl.PlaneTypeId).FirstOrDefault();
-                            dgv_Revenue.Rows.Add(stt, fl.FlightCode, fl.DateFlight, soveban, plane.TotalSeat, total);
-                            stt++;
                             i = j - 1;
                             break;
                         }
                     }
-                    if (i + 1 == ticket.Count)
+                    if (soveban > 1)
+                    {
+                        var fl = _flightServices.get_list().Where(c => c.Id == ticket[i].FlightId).FirstOrDefault();
+                        var plane = _planeTypeServices.get_list().Where(c => c.Id == fl.PlaneTypeId).FirstOrDefault();
+                        dgv_Revenue.Rows.Add(stt, fl.FlightCode, fl.DateFlight, soveban, plane.TotalSeat, total);
+                        stt++;
+                    }
+                    if (soveban == 1)
                     {
                         var fl = _flightServices.get_list().Where(c => c.Id == ticket[i].FlightId).FirstOrDefault();
                         var plane = _planeTypeServices.get_list().Where(c => c.Id == fl.PlaneTypeId).FirstOrDefault();
                         dgv_Revenue.Rows.Add(stt, fl.FlightCode, fl.DateFlight, 1, plane.TotalSeat, total);
                         stt++;
+                    }
+                    if (i + soveban == ticket.Count)
+                    {
+                        break;
                     }
                 }
             }
